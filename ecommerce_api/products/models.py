2a0d1a45f994bd5558435a58992f234db8dc1b2e
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-01 10:22:56 -0600

    added products migration

--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('created',)
